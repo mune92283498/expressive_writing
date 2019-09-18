a07b2cf924f31eb7147bbf74e6b570813704053f
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2019-09-19 00:07:41 +0900

    2019.09.18 削除機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
+  end
+
+  def create
+    User.create(user_pararms)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+  end
+
+  private
+  def user_pararms
+    params.require(:user).permit(:name, :age)
   end
 end
