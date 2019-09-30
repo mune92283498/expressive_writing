ffc3ba9e856f4819673bde118aa5e9186902883d
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2019-09-30 22:03:31 +0900

    jqueryのインストール。application.jsの追加修正。

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# require 'rails/commands/server'
+# module Rails
+#   class Server
+#     def default_options
+#       super.merge({
+#         :Port => 4000
+#         })
+#     end
+#   end
+# end
