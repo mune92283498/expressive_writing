963116c49d777da72ad3dff7f6229f4e0230c4ef
Masamune Hiroiwa <rider_jump_kick@yahoo.co.jp>
2019-10-04 23:26:00 +0900

    編集機能を実装

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -10,6 +10,22 @@ class EmotionsController < ApplicationController
     @emotion = Emotion.new
   end
 
+  def create
+    @emotion = Emotion.new(emotion_params)
+    if @emotion.save
+      redirect_to root_path, notice: '登録しました。'
+    else
+      flash[:alert] = '登録できませんでした。'
+      render :new
+    end
+  end
+
   def edit
   end
+
+  private 
+  def emotion_params
+    params.require(:emotion).permit(:emotion_name, :emotion_comment)
+  end
+
 end
--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,2 +1,4 @@
 class Emotion < ApplicationRecord
+  validates :emotion_name, presence: true
+  validates :emotion_comment, presence: true
 end
